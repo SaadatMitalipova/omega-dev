73beeb3e782995bf32af248e0bff50bde72ace4f
saadat <saadat0989@gmail.com>
2024-07-24 22:01:53 +0600

    Login and SignUp page UI finished

--- src/App.tsx
+++ src/App.tsx
@@ -6,52 +6,46 @@ import Video from "./pages/Video/Video";
 import Organizations from "./pages/Organizations/Organizations";
 import Community from "./pages/Сommunity/Community";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import ProtectedRoute from "./Companent/ProtectedRoute";
 import Login from "./Companent/Login";
 import Signup from "./Companent/SignUp";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-    },
-    {
-        path: "/vacancies",
-        element: (
-            <ProtectedRoute>
-                <Vacancies />
-            </ProtectedRoute>
-        ),
-    },
-    {
-        path: "/events",
-        element: <Events />,
-    },
-    {
-        path: "/video",
-        element: <Video />,
-    },
-    {
-        path: "/organizations",
-        element: <Organizations />,
-    },
-    {
-        path: "/community",
-        element: <Community />,
-    },
-    {
-        path: "/login",
-        element: <Login />
-    },
-    {
-        path: '/signup',
-        element: <Signup />
-    }
-  
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/vacancies",
+    element: <Vacancies />,
+  },
+  {
+    path: "/events",
+    element: <Events />,
+  },
+  {
+    path: "/video",
+    element: <Video />,
+  },
+  {
+    path: "/organizations",
+    element: <Organizations />,
+  },
+  {
+    path: "/community",
+    element: <Community />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
 ]);
 
 function App() {
-    return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/Companent/Login/index.tsx
+++ src/Companent/Login/index.tsx
@@ -1,77 +1,90 @@
-import { useState } from 'react';
-import Cookies from 'js-cookie'
-import axios from 'axios';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import "..//Login/login.css";
+import Cookies from "js-cookie";
+import axios from "axios";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { useNavigate } from "react-router-dom";
 
-
-const loginAPI = 'http://3.38.98.134/auth/login';
-
+const loginAPI = "http://3.38.98.134/auth/login";
 
 const Login = () => {
   const navigate = useNavigate();
-  const [userName, setUserName] = useState('');
-  const [password, setPassword]= useState('');
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLogin = async(e: React.FormEvent) => {
+  const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
       const response = await axios.post(loginAPI, {
         userName: userName,
         password: password,
-      })
-      if (response.data.success){
-        Cookies.set('token', response.data.token, { expires: 7 });
-        navigate('/');
+      });
+      if (response.data.success) {
+        Cookies.set("token", response.data.token, { expires: 7 });
+        navigate("/");
       }
       alert(response.data.message);
-    }
-    catch (error) {
-      console.error('Login kilganda error chikti', error);
+    } catch (error) {
+      console.error("Login kilganda error chikti", error);
       alert("Login bolbodu");
     }
-
-  }
+  };
 
   return (
     <>
-    <Header />
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100vh'
-    }}>
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-          <input
-            type="text"
-            id="username"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            name="username"
-            placeholder="Username"
-          />
-          <input 
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            name="password"
-            placeholder="Password" />
-          <button type="submit">Login</button>
-      </form>
-      <div style={{
-        marginTop: '20px'
-      }}>
-        <h1>Not Logged In?</h1>
-        <a href='/signup'> Sign up</a>
+      <Header />
+      <div className="container">
+        <div className="login-content">
+          <div className="login">
+            <h1>Login</h1>
+            <form onSubmit={handleLogin}>
+              <input
+                type="text"
+                id="username"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                name="username"
+                placeholder="Username"
+              />
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                name="password"
+                placeholder="Password"
+              />
+              <button className="btn-login" type="submit">
+                Login
+              </button>
+            </form>
+            <div
+              style={{
+                marginTop: "20px",
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <h1 className="title-NotLoggedIn">Not Logged In?</h1>
+              <a
+                style={{
+                  fontSize: 20,
+                  paddingTop: 10,
+                }}
+                href="/signup"
+              >
+                {" "}
+                Sign up
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
-  )
-}
+  );
+};
 export default Login;
